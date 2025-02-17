18aa4b00ab4480af159e81a2485ed208a0b7034a
Fatra <arielyfatra@gmail.com>
2025-02-17 11:34:00 +0700

    fix: container

--- template/resources/views/user/explore.blade.php
+++ template/resources/views/user/explore.blade.php
@@ -10,7 +10,7 @@
 
 @section('content')
   <!-- content -->
-  <div class="container">
+  <div class="container-fluid">
         <div class="header">
           <h3>Discover the best of Pixavault</h3>
         </div>
